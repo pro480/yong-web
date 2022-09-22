453b9ea250112e60223c94fda18254f8a7f5766b
cys0926 <108395686+cys0926@users.noreply.github.com>
2022-09-22 14:35:51 +0900

    푸쉬 권한 테스트왜돼냐2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>Hello, World 지라 테스트1 푸시 권한 테스트 윤석 이게 왜돼</div>
+    <div>Hello, World 지라 테스트1 푸시 권한 테스트 윤석 이게 왜돼냐</div>
   )
 }
 
