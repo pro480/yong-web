b6b09114237982cb46e4f484ca1f230cbaf5ddb5
cys0926 <108395686+cys0926@users.noreply.github.com>
2022-10-04 20:24:24 +0900

    LY-34 주요 업무 소개 페이지 제작

--- pages/info/task.tsx
+++ pages/info/task.tsx
@@ -12,7 +12,35 @@ function Task() {
                 secondDepth='주요 수행 업무'
                 secondLink='/info/task'
             />
-            <div>dsd</div>
+            <div className='flex w-full py-12'>
+                <div className='h-44 w-44 border border-8 p-5'>
+                    <p className='text-lg font-semibold underline underline-offset-4'>
+                        NO 1
+                    </p>
+                    <p className='mt-1 text-2xl font-semibold'>업무 1</p>
+                </div>
+                <div className='ml-10'>내용을 입력해주세요</div>
+            </div>
+            <hr className='border-dashed border-gray-400' />
+            <div className='flex h-full py-12'>
+                <div className='h-44 w-44 border border-8 p-5'>
+                    <p className='text-lg font-semibold underline underline-offset-4'>
+                        NO 2
+                    </p>
+                    <p className='mt-1 text-2xl font-semibold'>업무 2</p>
+                </div>
+                <div className='ml-10'>내용을 입력해주세요</div>
+            </div>
+            <hr className='border-dashed border-gray-400' />
+            <div className='flex h-full py-12'>
+                <div className='h-44 w-44 border border-8 p-5'>
+                    <p className='text-lg font-semibold underline underline-offset-4'>
+                        NO 3
+                    </p>
+                    <p className='mt-1 text-2xl font-semibold'>업무 3</p>
+                </div>
+                <div className='ml-10'>내용을 입력해주세요</div>
+            </div>
         </div>
     );
 }
--- pages/info/vision/index.tsx
+++ pages/info/vision/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import PageTitle from "../../../src/components/PageTitle";
 
 function Index() {
-    return <div>센터 비전 페이지입니다.</div>;
+    return (
+        <div>
+            <PageTitle
+                title='센터 비전'
+                description='센터 비전 페이지의 설명을 입력해주세요.'
+                firstDepth='센터 소개'
+                firstLink='/info'
+                secondDepth='센터 비전'
+                secondLink='/info/vision'
+            />
+        </div>
+    );
 }
 
 export default Index;
