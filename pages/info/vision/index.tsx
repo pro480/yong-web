a815dbd8f0bcc3120b492c90d202eb862082439f
BlowUP1997 <sksmsal97@gmail.com>
2022-10-06 23:35:23 +0900

    센터비전페이지.수정1

--- pages/info/vision/index.tsx
+++ pages/info/vision/index.tsx
@@ -13,41 +13,89 @@ function Index() {
                 secondLink='/info/vision'
             />
 
+            {/* 비전 제목 */}
+            <div className = "pb-12">
+                <h1 className='h-full whitespace-nowrap border-b border-b-PRIMARY_COLOR pb-2 text-2xl font-semibold text-PRIMARY_COLOR'>
+                        Center Vision
+                </h1>
+            </div>
+
             {/* 비전 이미지 */}
-            <div className = "flex justify-center">
-                <Image
-                    src='https://bit.ly/3MfbAnQ'
-                    width={800}
-                    height={350}
-                    alt='센터 비전 이미지'
-                    className=""
-                />
-                <p className = "flex absolute h-[150px] items-center justify-start text-2xl font-bold">
-                    비전 및 기능 예시 이미지 입니다
+            <div className = "flex justify-center h-[400px] w-[1000px] ml-12 rounded-[70px] bg-gray-200 ">
+                <p className = "flex items-center text-2xl font-bold">
+                    비전 이미지 입니다
                 </p>
             </div>
 
             {/* 비전 설명 1, 2 ,3  */}
             <div className ="flex justify-center py-10 space-x-[50px] text-xl font-semibold">
-                <div className = "box-content h-[400px] w-[280px] rounded-[70px] bg-[#7EB92C]">
+                <div className = "box-content h-[400px] w-[300px] rounded-[70px] bg-gray-300">
                     <p className = "grid place-items-center py-[180px]">
-                        비전 * 기능 내용 1
+                        비전 설명 1
                     </p>
                 </div>
 
-                <div className = "box-content h-[400px] w-[280px] rounded-[70px] bg-[#21B0A3]">
+                <div className = "box-content h-[400px] w-[300px] rounded-[70px] bg-gray-400">
                     <p className = "grid place-items-center py-[180px]">
-                        비전 * 기능 내용 2
+                        비전 설명 2
                     </p>
                 </div>
 
-                <div className = "box-content h-[400px] w-[280px] rounded-[70px] bg-[#1D9EC0]">
+                <div className = "box-content h-[400px] w-[300px] rounded-[70px] bg-gray-500">
                     <p className = "grid place-items-center py-[180px]">
-                        비전 * 기능 내용 3
+                        비전 설명 3
                     </p>
                 </div>
-
             </div>
+
+            {/* 주요 기능 제목 */}
+            <div className = "pb-12">
+                <h1 className='h-full whitespace-nowrap border-b border-b-PRIMARY_COLOR pb-2 text-2xl font-semibold text-PRIMARY_COLOR'>
+                        Main Function
+                </h1>
+            </div>
+
+            {/* 주요 기능 이미지 */}
+            <div className = "flex justify-center h-[400px] w-[1000px] ml-12 rounded-[70px] bg-gray-200 ">
+                <p className = "flex items-center text-2xl font-bold">
+                    주요 기능 이미지 입니다
+                </p>
+            </div>
+
+            {/* 주요 기능 설명 */}
+            <div className="flex-col space-y-5 justify-center ml-12 mt-12 text-xl font-semibold text-left">
+                {/* 기능 이름 */}
+                    {/* 기능 설명 */}
+                <div className = "flex-col">
+                    <span className = "border-b-black border-b-[3px]"> &nbsp; 기능 이름 1 &nbsp;</span>
+                    <div className = "flex justify-center pt-5 h-[100px] border-b-gray border-b-[2px] rounded-lg">
+                        기능 설명을 입력해주세요
+                    </div>
+                </div>
+
+                <div className = "flex-col">
+                    <span className = "border-b-black border-b-[3px]"> &nbsp; 기능 이름 2 &nbsp;</span>
+                    <div className = "flex justify-center pt-5 h-[100px] border-b-gray border-b-[2px] rounded-lg">
+                        기능 설명을 입력해주세요
+                    </div>
+                </div>
+
+                <div className = "flex-col">
+                    <span className = "border-b-black border-b-[3px]"> &nbsp; 기능 이름 3 &nbsp;</span>
+                    <div className = "flex justify-center pt-5 h-[100px] border-b-gray border-b-[2px] rounded-lg">
+                        기능 설명을 입력해주세요
+                    </div>
+                </div>
+
+                <div className = "flex-col">
+                    <span className = "border-b-black border-b-[3px]"> &nbsp; 기능 이름 4 &nbsp;</span>
+                    <div className = "flex justify-center pt-5 h-[100px] border-b-gray border-b-[2px] rounded-lg">
+                        기능 설명을 입력해주세요
+                    </div>
+                </div>
+            </div>
+        
+        
         </div>
 
     );
