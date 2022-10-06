e693797b8a62e5bf4f166449d4e003654758c0ce
BlowUP1997 <sksmsal97@gmail.com>
2022-10-06 22:20:42 +0900

    LY-46 센터 비전 페이지

--- pages/info/vision/index.tsx
+++ pages/info/vision/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PageTitle from "../../../src/components/PageTitle";
-
+import Image from "next/image"
 function Index() {
     return (
         <div>
@@ -12,7 +12,44 @@ function Index() {
                 secondDepth='센터 비전'
                 secondLink='/info/vision'
             />
+
+            {/* 비전 이미지 */}
+            <div className = "flex justify-center">
+                <Image
+                    src='https://bit.ly/3MfbAnQ'
+                    width={800}
+                    height={350}
+                    alt='센터 비전 이미지'
+                    className=""
+                />
+                <p className = "flex absolute h-[150px] items-center justify-start text-2xl font-bold">
+                    비전 및 기능 예시 이미지 입니다
+                </p>
+            </div>
+
+            {/* 비전 설명 1, 2 ,3  */}
+            <div className ="flex justify-center py-10 space-x-[50px] text-xl font-semibold">
+                <div className = "box-content h-[400px] w-[280px] rounded-[70px] bg-[#7EB92C]">
+                    <p className = "grid place-items-center py-[180px]">
+                        비전 * 기능 내용 1
+                    </p>
+                </div>
+
+                <div className = "box-content h-[400px] w-[280px] rounded-[70px] bg-[#21B0A3]">
+                    <p className = "grid place-items-center py-[180px]">
+                        비전 * 기능 내용 2
+                    </p>
+                </div>
+
+                <div className = "box-content h-[400px] w-[280px] rounded-[70px] bg-[#1D9EC0]">
+                    <p className = "grid place-items-center py-[180px]">
+                        비전 * 기능 내용 3
+                    </p>
+                </div>
+
+            </div>
         </div>
+
     );
 }
 
