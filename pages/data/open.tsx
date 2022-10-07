753ed1948d59ba2f6665b5f132f72dd749a774a1
cys0926 <108395686+cys0926@users.noreply.github.com>
2022-10-07 15:47:45 +0900

    mou 수정

--- pages/data/open.tsx
+++ pages/data/open.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Open() {
-    return <div>오픈 데이터 페이지입니다.</div>;
+    return (
+        <div>
+            <div className='h-fit w-fit border border-black'>
+                <div className=' flex h-32 w-80 items-center justify-center bg-gray-300'>
+                    로고이미지
+                </div>
+                <div className='flex h-12 w-full items-center justify-center'>
+                    링크 설명
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Open;
