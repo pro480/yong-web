6576be844f21e781c1bb9437c84b5a662433dc83
cys0926 <108395686+cys0926@users.noreply.github.com>
2022-10-07 20:36:16 +0900

    오픈데이터 수정

--- pages/data/open.tsx
+++ pages/data/open.tsx
@@ -1,65 +1,93 @@
 import React from "react";
 import PageTitle from "../../src/components/PageTitle";
-
+import Image from "next/image";
+import { ImGithub } from "react-icons/im";
 
 function Open() {
+    const opens = [
+        {
+            imgUrl: "https://www.nia.or.kr/img/nia_kor/ci_03.png",
+            href: "https://www.nia.or.kr/site/nia_kor/main.do",
+            title: "NIA : 한국지능정보사회진흥원",
+            description:
+                "대한민국 국가기관의 정보화 추진과 관련된 정책을\n" +
+                "개발, 정보문화 조성, 정보격차 해소 등을\n" +
+                "지원하려는 목적으로 만들어진 대한민국\n" +
+                "과학기술정보통신부 산하 위탁집행형 준정부기관",
+        },
+        {
+            imgUrl: "https://www.nia.or.kr/img/nia_kor/ci_03.png",
+            href: "https://www.nia.or.kr/site/nia_kor/main.do",
+            title: "NIA : 한국지능정보사회진흥원",
+            description:
+                "대한민국 국가기관의 정보화 추진과 관련된 정책을\n" +
+                "개발, 정보문화 조성, 정보격차 해소 등을\n" +
+                "지원하려는 목적으로 만들어진 대한민국\n" +
+                "과학기술정보통신부 산하 위탁집행형 준정부기관",
+        },
+        {
+            imgUrl: "https://www.nia.or.kr/img/nia_kor/ci_03.png",
+            href: "https://www.nia.or.kr/site/nia_kor/main.do",
+            title: "NIA : 한국지능정보사회진흥원",
+            description:
+                "대한민국 국가기관의 정보화 추진과 관련된 정책을\n" +
+                "개발, 정보문화 조성, 정보격차 해소 등을\n" +
+                "지원하려는 목적으로 만들어진 대한민국\n" +
+                "과학기술정보통신부 산하 위탁집행형 준정부기관",
+        },
+        {
+            imgUrl: "https://www.nia.or.kr/img/nia_kor/ci_03.png",
+            href: "https://www.nia.or.kr/site/nia_kor/main.do",
+            title: "NIA : 한국지능정보사회진흥원",
+            description:
+                "대한민국 국가기관의 정보화 추진과 관련된 정책을\n" +
+                "개발, 정보문화 조성, 정보격차 해소 등을\n" +
+                "지원하려는 목적으로 만들어진 대한민국\n" +
+                "과학기술정보통신부 산하 위탁집행형 준정부기관",
+        },
+    ];
+
     return (
         <div>
-
             <PageTitle
-                    title='오픈 데이터'
-                    description='오픈 데이터 페이지의 설명을 입력해주세요.'
-                    firstDepth='자료실'
-                    firstLink='/data'
-                    secondDepth='오픈 데이터'
-                    secondLink='/data/open'
-                />
-            <div className=" w-[1000px]">
-                <div className=" flex items-center mb-5 text-sm">사이트명을 클릭하시면 상세 내용을 확인하실 수 있습니다</div>
-                <div className="text-lg border-t-2 border-PRIMARY_COLOR">
-                    <div className=" h-12 flex items-center justify-between border-b-2 border-gray-300">
-                        <div>사이트명1</div>
-                        <div>다운 아이콘</div>
-                    </div>
-                    <div className=" h-12 flex items-center justify-between border-b-2 border-gray-300">
-                        <div>사이트명2</div>
-                        <div>다운 아이콘</div>
-                    </div>
-                    
-                    <div className=" h-12 flex items-center justify-between border-b-2 border-gray-300">
-                        <div>사이트명3</div>
-                        <div>다운 아이콘</div>
-                    </div>
-                    
-                    <div className=" h-12 flex items-center justify-between border-b-2 border-gray-300">
-                        <div>사이트명4</div>
-                        <div>다운 아이콘</div>
-                    </div>
-                    
-                    <div className=" h-12 flex items-center justify-between border-b-2 border-gray-300">
-                        <div>사이트명5</div>
-                        <div>다운 아이콘</div>
-                    </div>
-                    
-                    <div className=" h-12 flex items-center justify-between border-b-2 border-gray-300">
-                        <div>사이트명6</div>
-                        <div>다운 아이콘</div>
-                    </div>
-                    
-                    
-
-                </div>
-                
-                    
-            </div>
-
-            
-        
-    
-    
+                title='오픈 데이터'
+                description='오픈 데이터 페이지의 설명을 입력해주세요.'
+                firstDepth='자료실'
+                firstLink='/data'
+                secondDepth='오픈 데이터'
+                secondLink='/data/open'
+            />
+            <main className='grid grid-cols-1 items-center gap-y-16 gap-x-14 self-center md:grid-cols-2 xl:grid-cols-3'>
+                {opens.map((open) => (
+                    <article className='mx-auto h-96 w-80 border hover:scale-110'>
+                        <div className='h-[40%] border px-4'>
+                            <a
+                                className='relative block h-full cursor-pointer'
+                                href={open.href}
+                                target='_blank'
+                                rel='noopener noreferrer'
+                            >
+                                <Image
+                                    src={open.imgUrl}
+                                    layout='fill'
+                                    objectFit='contain'
+                                    objectPosition='center'
+                                    alt='로고'
+                                    priority={true}
+                                />
+                            </a>
+                        </div>
+                        <div className='flex h-[60%] flex-col items-center justify-center gap-y-4 bg-PRIMARY_COLOR/30  py-3 px-2'>
+                            <h1 className='text-lg font-semibold'>
+                                {open.title}
+                            </h1>
+                            <p className='text-sm'>{open.description}</p>
+                        </div>
+                    </article>
+                ))}
+            </main>
         </div>
-        )
-
+    );
 }
 
 export default Open;
