059c372375e2f7c599fef2f152d850ab1e2e3576
zelope <96818023+zelope@users.noreply.github.com>
2022-10-07 20:24:41 +0900

    finalLY28LY31

--- pages/data/open.tsx
+++ pages/data/open.tsx
@@ -1,23 +1,10 @@
 import React from "react";
+
 import PageTitle from "../../src/components/PageTitle";
 
 
 function Open() {
-    return (
-        <div>
-            <PageTitle
-                    title='오픈 데이터'
-                    description='오픈 데이터 페이지의 설명을 입력해주세요.'
-                    firstDepth='자료실'
-                    firstLink='/data'
-                    secondDepth='오픈 데이터'
-                    secondLink='/data/open'
-                />
-            <div className="w-2/3 h-[75px] bg-FOOTER_FONT_COLOR"> 이미지 컨테이너</div>
-            
-    
-        </div>
-        )
+    return 
 
 }
 
