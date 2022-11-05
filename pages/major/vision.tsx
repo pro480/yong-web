3e1295c961094e89cf419f5194ef8ebb1990b5ec
hyeono <abc716amazing@gmail.com>
2022-11-05 23:16:39 +0900

    LY-104졸업후진로페이지반응형추가수정

--- pages/major/vision.tsx
+++ pages/major/vision.tsx
@@ -12,7 +12,7 @@ function Vision() {
             <main className='relative flex flex-col gap-y-12'>
                 <div className={styles.outer}>
                     <div className={styles.inner}>
-                        <div className='relative hidden sm:flex w-36'>
+                        <div className='relative hidden items-center sm:flex w-36'>
                             <div className='absolute right-4 top-4 h-24 w-24 rounded-full bg-gray-300 p-6 '>
                                 <FaChalkboardTeacher className='h-full w-full' />
                             </div>
@@ -22,7 +22,8 @@ function Vision() {
                             <h1 className='xs:text-lg sm:text-xl md:text-2xl font-bold'>
                                 중등학교 교사
                             </h1>
-                            <p className='mt-1 xs:mt-2'>
+                            <div className="border-b"></div>
+                            <p className='mt-1 text-[0.925rem] xs:mt-2'>
                                 교육학 전공 또는 희망하는 교과목에 대해 부전공이나 
                                 복수전공을 하면 중등학교 2급 정교사 자격증을 얻을 수 있다. 
                                 이후 임용고시를 통과하거나 소정의 공채과정을 통해서 중등학교의 
@@ -43,7 +44,8 @@ function Vision() {
                             <h1 className='xs:text-lg sm:text-xl md:text-2xl font-bold'>
                                 대학교수 및 교육연구전문가
                             </h1>
-                            <p className='mt-1 xs:mt-2'>
+                            <div className="border-b"></div>
+                            <p className='mt-1 text-[0.925rem] xs:mt-2'>
                                 교육학은 다른 인문학 관련 학과에 비해서 대학과 연구기관의
                                 수요가 많은 편이므로,대학원을 진학해서 박사학위를 취득한 후 
                                 대학교수가 되거나 교육연구기관의 연구원이 되는 것이 이 분야의 
@@ -64,7 +66,8 @@ function Vision() {
                             <h1 className='xs:text-lg sm:text-xl md:text-2xl font-bold'>
                                 기업체의 HRD 전문가
                             </h1>
-                            <p className='mt-1 xs:mt-2'>
+                            <div className="border-b"></div>
+                            <p className='mt-1 text-[0.925rem] xs:mt-2'>
                                 HRD 및 교육공학 관련 교과목들을 통해 최근
                                 기업체에서 활성화되고 있는 인적자원개발교육, 연수 
                                 교육에 대한 교육적 지식을 쌓고, 또한 대학원에 진학하여 
@@ -85,7 +88,8 @@ function Vision() {
                             <h1 className='xs:text-lg sm:text-xl md:text-2xl font-bold'>
                                 교육공무원 및 교육행정전문가
                             </h1>
-                            <p className='mt-1 xs:mt-2'>
+                            <div className="border-b"></div>
+                            <p className='mt-1 text-[0.925rem] xs:mt-2'>
                                 교육 분야 행정고등고시, 공무원공채를 통해서
                                 교육행정전문가로 진출할 수 있다. 최근 전문적인
                                 지식을 갖춘 대학행정직원이 필요성이 대두됨에 따라
@@ -106,7 +110,8 @@ function Vision() {
                             <h1 className='xs:text-lg sm:text-xl md:text-2xl font-bold'>
                                 상담전문가
                             </h1>
-                            <p className='mt-1 xs:mt-2'>
+                            <div className="border-b"></div>
+                            <p className='mt-1 text-[0.925rem] xs:mt-2'>
                                 상담관련 과목의 이수를 통해 상담자로서의 전문적 자질과 
                                 능력을 갖춘다면 국가차원의 청소년상담시설, 기업체의 
                                 상담관련부서, 대학의 상담기관, 사설상담기관 등에서 
