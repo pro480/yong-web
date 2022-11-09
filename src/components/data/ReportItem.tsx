23512e4d2d2042a29f66f2b586fdb8e7faa3ad00
cookie0010 <sg.been2017@gmail.com>
2022-11-09 12:34:24 +0900

    연구 보고서 반응형

--- src/components/data/ReportItem.tsx
+++ src/components/data/ReportItem.tsx
@@ -10,7 +10,7 @@ export default function ReportItem() {
 
     return (
         <React.Fragment>
-            <div className='my-5 grid grid-cols-1 items-center gap-y-16 gap-x-1 self-center md:grid-cols-2 xl:grid-cols-4'>
+            <div className='my-5 grid grid-cols-2 items-center gap-y-16 gap-x-1 self-center lg:grid-cols-3 xl:grid-cols-4'>
                 {researchList
                     ?.sort(
                         (a, b) =>
@@ -22,9 +22,9 @@ export default function ReportItem() {
                         return (
                             <div
                                 key={docSnapshot.id}
-                                className=' flex h-[500px] w-[250px] flex-col justify-between'
+                                className='flex h-[340px] w-[170px] flex-col justify-between sm:h-[440px] sm:w-[220px] md:h-[480px] md:w-[240px] xl:h-[500px] xl:w-[250px]'
                             >
-                                <div className=' relative h-[352px]  '>
+                                <div className=' relative h-full  xl:h-[352px] '>
                                     <Image
                                         src={items.imgUrl}
                                         layout='fill'
@@ -36,29 +36,29 @@ export default function ReportItem() {
                                 </div>
                                 <div className=' flex flex-col justify-around p-1'>
                                     <div className='py-1'>
-                                        <div className=' text-xl'>
+                                        <div className='sm:text-lg xl:text-xl'>
                                             {items.title}
                                         </div>
-                                        <div className=' text-lg'>
+                                        <div className='text-sm sm:text-base xl:text-lg'>
                                             {items.type}
                                         </div>
                                     </div>
                                     <div className='flex justify-between'>
                                         <div className='py-1'>
-                                            <div className=' text-sm text-GRAY_COLOR-500'>
+                                            <div className=' text-[11px] text-GRAY_COLOR-500 sm:text-[13px] xl:text-sm'>
                                                 연구자 |{" "}
                                                 <span>
                                                     {" "}
                                                     {items.researcher}{" "}
                                                 </span>
                                             </div>
-                                            <div className=' text-sm text-GRAY_COLOR-500'>
+                                            <div className='text-[11px] text-GRAY_COLOR-500 sm:text-[13px] xl:text-sm'>
                                                 발행 년도 |{" "}
                                                 <span>{items.year}</span>
                                             </div>
                                         </div>
                                         <a
-                                            className='mx-2 h-[30px] w-[40px] cursor-pointer bg-PRIMARY_COLOR-300 text-center text-white hover:bg-PRIMARY_COLOR-600'
+                                            className='mx-2 h-[24px] w-[30px] cursor-pointer bg-PRIMARY_COLOR-300 text-center text-[13px] text-white hover:bg-PRIMARY_COLOR-600 sm:h-[30px] sm:w-[40px] sm:text-base xl:h-[30px] xl:w-[40px] xl:text-base'
                                             href={items.fileUrl}
                                         >
                                             <span>pdf</span>
