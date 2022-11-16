040a741d81c6667e4f12de00e0b2e780cdcfdde0
zelope <96818023+zelope@users.noreply.github.com>
2022-11-16 16:16:20 +0900

    메인페이지 반응형 최종

--- src/components/main/MainForum.tsx
+++ src/components/main/MainForum.tsx
@@ -16,6 +16,7 @@ import { Snapshot } from "recoil";
 import { data } from "../../utils/Utils";
 import Image from "next/image";
 import { Media } from "reactstrap";
+import moment from "moment";
 
 function MainForum() {
     const ref = query(
@@ -25,7 +26,7 @@ function MainForum() {
     );
     const collectionQuery = useFirestoreQuery(["gallery_createdAt"], ref);
     return (
-        <div className='flex  h-[500px] flex-col  gap-y-10 p-10 lg:h-[600px] desktop:h-[700px]'>
+        <div className='flex  h-[500px] flex-col gap-y-10 p-10  xs:h-[400px] md:h-[500px] lg:h-[600px] desktop:h-[700px]'>
             <div className='flex h-[10%] w-full items-center justify-center text-lg font-bold  xs:text-xl  md:text-2xl lg:text-3xl  desktop:text-4xl'>
                 빅데이터 연구센터 갤러리
                 <Link href='/data/gallery'>
@@ -35,34 +36,39 @@ function MainForum() {
                     </div>
                 </Link>
             </div>
-            <div className='flex flex-col  justify-center gap-y-4 md:flex-row md:gap-y-0 md:gap-x-8'>
+            <div className='flex flex-col  justify-center gap-y-4 xs:flex-row xs:gap-y-0 xs:gap-x-4 md:gap-x-8'>
                 {collectionQuery.data?.docs.map((snapshot) => {
                     const data = snapshot.data();
                     return (
                         <div
-                            className=' flex h-[160px] w-[100%] flex-col gap-y-4  border-2 border-PRIMARY_COLOR-600 p-3 shadow-md shadow-PRIMARY_COLOR-600  md:h-[300px] md:w-[47%]  lg:h-[400px] desktop:h-[500px]  '
+                            className=' flex h-[160px] w-[100%] flex-col gap-y-4  xs:h-[200px] border-2 border-PRIMARY_COLOR-600 p-3 shadow-md shadow-PRIMARY_COLOR-600  xs:w-[47%] md:h-[300px]  lg:h-[400px] desktop:h-[500px]  '
                             key={snapshot.id}
+                            
                         >
-                            <div className='  hidden justify-between md:flex     '>
-                                <p className='overflow-hidden text-ellipsis whitespace-pre border-b-2 border-PRIMARY_COLOR-300 px-2  text-sm font-semibold  md:text-sm desktop:text-lg'>
-                                    {data.title}
-                                </p>
-                                <div className=' flex items-center text-xs text-GRAY_COLOR-700'>
-                                    <ClockIcon className=' h-6 md:h-4 ' />
-                                    {data.createdAt}
-                                </div>
-                            </div>
-                            <div className='relative  h-full w-full '>
+                            <div className='   relative aspect-[4/3] h-[100%] '>
                                 <Image
+                                    className=''
                                     src={data.imgUrl}
                                     layout='fill'
-                                    objectFit='contain'
+                                    objectFit='cover'
                                     objectPosition={"top"}
                                     alt='연구 보고서'
                                     priority={true}
                                     quality={50}
                                 />
                             </div>
+                            <div className='  flex justify-between     '>
+                                <p className=' w-full overflow-hidden text-ellipsis whitespace-pre border-b-2 border-PRIMARY_COLOR-300 px-2 text-sm  font-semibold md:w-1/2  md:text-sm desktop:text-lg'>
+                                    {data.title}
+                                </p>
+                                <div className=' hidden items-center text-GRAY_COLOR-700 md:flex md:text-xs lg:text-sm desktop:text-base'>
+                                    <ClockIcon className=' md:h-4 lg:h-6 ' />
+                                    {moment(data.createdAt.slice(0, 8)).format(
+                                        "YYYY-MM-DD"
+                                    )}
+                                </div>
+                            </div>
+                            
                         </div>
                     );
                 })}
